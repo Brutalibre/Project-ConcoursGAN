2f320384d7733eb23083ebae14572e5ba23b6455
Elinion <alexandre.abts@gmail.com>
2015-10-20 19:09:47 +0900

    Continued YELLOW part
    added a vertical tunnel with some spikes

--- UnityProject/Assets/Scripts/MovingPlatform.cs
+++ UnityProject/Assets/Scripts/MovingPlatform.cs
@@ -8,17 +8,26 @@ public class MovingPlatform : MonoBehaviour
     public float waitTimeAtStart = 0f;
     public Transform end;
     public float waitTimeAtEnd = 0f;
+    public float waitTimeBeforeStart = 0f;
+    public bool waitForPlayer = false;
 
     private bool towardsEnd = true;
     private float margin = .1f;
     private Rigidbody rb;
     private float waitTime = 0f;
+    private bool isStarted = false;
 
     void Awake()
     {
         transform.position = start.position;
         waitTime = waitTimeAtStart;
         rb = GetComponent<Rigidbody>();
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == Tags.Player)
+            waitForPlayer = false;
     }
 
     void Update()
@@ -34,9 +43,23 @@ public class MovingPlatform : MonoBehaviour
 
     void FixedUpdate()
     {
+        // Do not move is the platform should wait for the player
+        if (waitForPlayer)
+            return;
+
+        // Do not move before the time set in the editor
+        if (!isStarted)
+        {
+            CheckStartTime();
+            return;
+        }
+
         // Check if platform is paused
         if (waitTime > 0)
+        {
+            rb.velocity = Vector3.zero;
             return;
+        }
 
         Vector3 targetPos = towardsEnd ? end.position : start.position;
         Vector3 moveVector = targetPos - transform.position;
@@ -55,6 +78,21 @@ public class MovingPlatform : MonoBehaviour
         }
         // Otherwise move towards the target
         else
-            rb.velocity = moveVector.normalized *  Time.deltaTime * moveSpeed * rb.mass;
+            rb.velocity = moveVector.normalized *  Time.deltaTime * moveSpeed;
+    }
+
+    bool CheckStartTime()
+    {
+        if (waitTimeBeforeStart < 0)
+        {
+        }
+        if (waitTimeBeforeStart < Time.timeSinceLevelLoad)
+        {
+            rb.isKinematic = false;
+            isStarted = true;
+            return true;
+        }
+            
+        return false;
     }
 }
