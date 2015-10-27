536fb972b7144f9253395b94292461e534a9a3c4
brutalibre <val.isautier@gmail.com>
2015-10-27 15:40:07 +0100

    Aller-retour bleu + visibilité entrée & sortie du tunnel
    TODO
    - Réussir à faire sortir le joueur du tunnel :(

--- UnityProject/Assets/Scripts/Color Fragments/VioletColorPick.cs
+++ UnityProject/Assets/Scripts/Color Fragments/VioletColorPick.cs
@@ -3,11 +3,13 @@ using System.Collections;
 using System;
 
 public class VioletColorPick : ColorPick {
-    
-	
+
+    public GameObject boulders;
+    public GameObject platforms;
 
     protected override void OnColorPick()
     {
-        
+        boulders.SetActive(true);
+        platforms.SetActive(true);
     }
 }
