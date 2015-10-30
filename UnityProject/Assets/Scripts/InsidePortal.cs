52a0f215e74c7fa7bc0289906c4c2fba5480c20c
brutalibre <val.isautier@gmail.com>
2015-10-30 23:34:30 +0100

    Implemented violet portals
    DONE:
    - Violet portals : easier to make than 3D platforms (not much time
    remaining).
    ==> Problem : player bugs when exiting (he stays stuck and cannot go
    back inside the portal)
    - Broke the Blue Path prefab : every blue path was broken (keypoints
    dédoublés n'importe où et carrés bleus déplacés systématiquements)
    
    TODO:
    - Continuer de placer les portails
    - Réfléchir aux menus et dialogues
    - Coder scenes de menu

---
+++ UnityProject/Assets/Scripts/InsidePortal.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InsidePortal : MonoBehaviour {
+
+    public bool insidePortal = false;
+    public Player player;
+    public GameObject entrance;
+    public GameObject exit;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CheckDoors();
+	}
+
+    public void GoToOtherDoor(GameObject departure)
+    {
+        if (!insidePortal)
+        {
+            if (departure == entrance)
+                player.transform.position = entrance.transform.position;
+            else
+                player.transform.position = exit.transform.position;
+
+            insidePortal = true;
+        }
+    }
+
+    public void CheckDoors()
+    {
+        if (entrance.GetComponent<Teleport>().inDoor || exit.GetComponent<Teleport>().inDoor)
+        {
+            insidePortal = true;
+        }
+
+        else insidePortal = false;
+    }
+}
---
+++ UnityProject/Assets/Scripts/Teleport.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleport : MonoBehaviour
+{
+
+    public bool inDoor = false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == Tags.Player)
+        {
+            Debug.Log("player enter");
+            inDoor = true;
+            GetComponentInParent<InsidePortal>().GoToOtherDoor(GetComponent<GameObject>());
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == Tags.Player)
+        {
+            inDoor = false;
+        }
+    }
+}
